b9d81f896ab129271e9f55b83f746ca7eb8f1e06
muiz sohani <muiz.sohani@ontario.ca>
2020-03-25 15:18:35 -0400

    hard coded chart :(

--- src/components/positive.jsx
+++ src/components/positive.jsx
@@ -6,173 +6,225 @@ class Positive extends Component {
     super(props);
     this.makeChart = this.makeChart.bind(this);
   }
+  state = {
+    datez: [],
+    confPos: []
+  };
   componentDidMount() {
     this.makeChart();
   }
 
   makeChart() {
-    var container = document.getElementById("positive");
-    var data = {
-      categories: [
-        "01/01/2016",
-        "02/01/2016",
-        "03/01/2016",
-        "04/01/2016",
-        "05/01/2016",
-        "06/01/2016",
-        "07/01/2016",
-        "08/01/2016",
-        "09/01/2016",
-        "10/01/2016",
-        "11/01/2016",
-        "12/01/2016"
-      ],
-      series: [
-        {
-          name: "Seoul",
-          data: [
-            -3.5,
-            -1.1,
-            4.0,
-            11.3,
-            17.5,
-            21.5,
-            24.9,
-            25.2,
-            20.4,
-            13.9,
-            6.6,
-            -0.6
-          ]
-        },
-        {
-          name: "Seattle",
-          data: [
-            3.8,
-            5.6,
-            7.0,
-            9.1,
-            12.4,
-            15.3,
-            17.5,
-            17.8,
-            15.0,
-            10.6,
-            6.4,
-            3.7
-          ]
-        },
-        {
-          name: "Sydney",
-          data: [
-            22.1,
-            22.0,
-            20.9,
-            18.3,
-            15.2,
-            12.8,
-            11.8,
-            13.0,
-            15.2,
-            17.6,
-            19.4,
-            21.2
-          ]
-        },
-        {
-          name: "Moskva",
-          data: [
-            -10.3,
-            -9.1,
-            -4.1,
-            4.4,
-            12.2,
-            16.3,
-            18.5,
-            16.7,
-            10.9,
-            4.2,
-            -2.0,
-            -7.5
-          ]
-        },
-        {
-          name: "Jungfrau",
-          data: [
-            -13.2,
-            -13.7,
-            -13.1,
-            -10.3,
-            -6.1,
-            -3.2,
-            0.0,
-            -0.1,
-            -1.8,
-            -4.5,
-            -9.0,
-            -10.9
-          ]
-        }
-      ]
-    };
-    var options = {
-      chart: {
-        width: 1160,
-        height: 540,
-        title: "24-hr Average Temperature"
-      },
-      yAxis: {
-        title: "Temperature (Celsius)"
-      },
-      xAxis: {
-        title: "Month",
-        pointOnColumn: true,
-        dateFormat: "MMM",
-        tickInterval: "auto"
-      },
-      series: {
-        showDot: false,
-        zoomable: true
-      },
-      tooltip: {
-        suffix: "°C"
-      }
-      //   plot: {
-      //     // bands: [
-      //     //   {
-      //     //     range: ["03/01/2016", "05/01/2016"],
-      //     //     color: "gray",
-      //     //     opacity: 0.2
-      //     //   }
-      //     // ],
-      //     lines: [
-      //       {
-      //         value: "03/01/2016",
-      //         color: "#fa2828"
-      //       }
-      //     ]
-      //   }
-    };
-    var theme = {
-      series: {
-        colors: [
-          "#83b14e",
-          "#458a3f",
-          "#295ba0",
-          "#2a4175",
-          "#289399",
-          "#289399",
-          "#617178",
-          "#8a9a9a",
-          "#516f7d",
-          "#dddddd"
+    var dates = [
+      "2020-01-28",
+      "2020-01-29",
+      "2020-01-30",
+      "2020-01-31",
+      "2020-02-01",
+      "2020-02-02",
+      "2020-02-03",
+      "2020-02-04",
+      "2020-02-05",
+      "2020-02-06",
+      "2020-02-07",
+      "2020-02-08",
+      "2020-02-09",
+      "2020-02-10",
+      "2020-02-11",
+      "2020-02-12",
+      "2020-02-13",
+      "2020-02-14",
+      "2020-02-15",
+      "2020-02-16",
+      "2020-02-17",
+      "2020-02-18",
+      "2020-02-19",
+      "2020-02-20",
+      "2020-02-21",
+      "2020-02-22",
+      "2020-02-23",
+      "2020-02-24",
+      "2020-02-25",
+      "2020-02-26",
+      "2020-02-27",
+      "2020-02-28",
+      "2020-02-29",
+      "2020-03-01",
+      "2020-03-02",
+      "2020-03-03",
+      "2020-03-04",
+      "2020-03-05",
+      "2020-03-06",
+      "2020-03-07",
+      "2020-03-08",
+      "2020-03-09",
+      "2020-03-10",
+      "2020-03-11",
+      "2020-03-12",
+      "2020-03-13"
+    ];
+    var confPos = [
+      1,
+      2,
+      2,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      1,
+      0,
+      1,
+      1,
+      1,
+      2,
+      3,
+      15,
+      17,
+      17,
+      18,
+      22,
+      24,
+      25,
+      31,
+      31,
+      37,
+      54,
+      74,
+      98,
+      140,
+      172,
+      183,
+      208,
+      251,
+      311,
+      369,
+      412,
+      489,
+      572
+    ];
+
+    // const csv = csv("covidtesting.csv");
+    // for (var i = 0; i < csv.length; i++) {
+    //   var points = csv.split(",");
+    //   dates[i] = points[0];
+    //   confPos[i] = points[5];
+    // }
+
+    setTimeout(() => {
+      var container = document.getElementById("positive");
+      var conf = confPos;
+      var datez = dates;
+      var data = {
+        categories: datez,
+        series: [
+          {
+            name: "Confirmed Positives",
+            data: conf
+          },
+          {
+            name: "Seattle",
+            data: [
+              3.8,
+              5.6,
+              7.0,
+              9.1,
+              12.4,
+              15.3,
+              17.5,
+              17.8,
+              15.0,
+              10.6,
+              6.4,
+              3.7
+            ]
+          },
+          {
+            name: "Sydney",
+            data: [
+              22.1,
+              22.0,
+              20.9,
+              18.3,
+              15.2,
+              12.8,
+              11.8,
+              13.0,
+              15.2,
+              17.6,
+              19.4,
+              21.2
+            ]
+          },
+          {
+            name: "Moskva",
+            data: [
+              -10.3,
+              -9.1,
+              -4.1,
+              4.4,
+              12.2,
+              16.3,
+              18.5,
+              16.7,
+              10.9,
+              4.2,
+              -2.0,
+              -7.5
+            ]
+          },
+          {
+            name: "Jungfrau",
+            data: [
+              -13.2,
+              -13.7,
+              -13.1,
+              -10.3,
+              -6.1,
+              -3.2,
+              0.0,
+              -0.1,
+              -1.8,
+              -4.5,
+              -9.0,
+              -10.9
+            ]
+          }
         ]
-      }
-    };
-    // For apply theme
-    // tui.chart.registerTheme('myTheme', theme);
-    // options.theme = 'myTheme';
-    chart.lineChart(container, data, options);
+      };
+      var options = {
+        chart: {
+          width: 1160,
+          height: 540,
+          title: "24-hr Average Temperature"
+        },
+        yAxis: {
+          title: "Temperature (Celsius)"
+        },
+        xAxis: {
+          title: "Month",
+          pointOnColumn: true,
+          dateFormat: "MMM",
+          tickInterval: "auto"
+        },
+        series: {
+          showDot: false,
+          zoomable: true
+        },
+        tooltip: {
+          suffix: "°C"
+        }
+      };
+      chart.lineChart(container, data, options);
+    }, 0.001);
   }
 
   render() {
