5228976bd74e593230bbbdd09ef24b2e0444abcf
muiz sohani <muiz.sohani@ontario.ca>
2020-03-26 13:56:11 -0400

    moved data parsing to parent component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import "./App.css";
 import Positive from "./components/positive.jsx";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.dataParse = this.dataParse.bind(this);
+    this.storeData = this.storeData.bind(this);
+  }
+  state = {};
+
+  componentDidMount() {
+    this.dataParse();
+  }
+
+  dataParse() {
+    var csv = require("./components/covidtesting.csv");
+    var Papa = require("papaparse/papaparse.min.js");
+    Papa.parse(csv, {
+      download: true,
+      complete: this.storeData
+    });
+  }
+
+  storeData(results) {
+    let parsedD = results.data;
+    this.setState({ data: parsedD });
+  }
   render() {
-    return <Positive />;
+    return <Positive data={this.state.data} />;
   }
 }
 
--- src/components/positive.jsx
+++ src/components/positive.jsx
@@ -5,112 +5,103 @@ import chart from "tui-chart";
 class Positive extends Component {
   constructor(props) {
     super(props);
-    this.dataParse = this.dataParse.bind(this);
     this.makeChart = this.makeChart.bind(this);
   }
-  state = {
-    datez: [],
-    confPos: []
-  };
+  state = {};
+
   componentDidMount() {
-    this.dataParse();
+    this.makeChart();
   }
 
-  dataParse() {
-    var csv = require("./covidtesting.csv");
-    var Papa = require("papaparse/papaparse.min.js");
-    Papa.parse(csv, {
-      download: true,
-      complete: this.makeChart
-    });
-  }
-
-  makeChart(results) {
-    const data = results.data;
-
-    var dates = [];
-
-    var resolved = [];
-    var confPos = [];
-    var totaltest = [];
-    var dead = [];
-
-    for (var i = 1; i < data.length - 1; i++) {
-      var row = data[i];
-
-      dates[i] = row[0];
-
-      if (!row[5]) {
-        confPos[i] = 0;
-      } else {
-        confPos[i] = row[5];
-      }
-      if (!row[6]) {
-        resolved[i] = 0;
-      } else {
-        resolved[i] = row[6];
-      }
-      if (!row[8]) {
-        totaltest[i] = 0;
-      } else {
-        totaltest[i] = row[8];
-      }
-      if (!row[7]) {
-        dead[i] = 0;
-      } else {
-        dead[i] = row[7];
-      }
-      console.log(row);
-    }
-
+  makeChart() {
     setTimeout(() => {
-      var container = document.getElementById("positive");
+      this.setState({ data: this.props.data });
+      const data = this.state.data;
 
-      var data = {
-        categories: dates,
-        series: [
-          {
-            name: "Total Tested",
-            data: totaltest
-          },
-          {
-            name: "Confirmed Positives",
-            data: confPos
-          },
-          {
-            name: "Recovered",
-            data: resolved
-          },
-          {
-            name: "Deaths",
-            data: dead
-          }
-        ]
-      };
-      var options = {
-        chart: {
-          width: 1160,
-          height: 540,
-          title: "Status of COVID-19 cases in Ontario"
-        },
-        yAxis: {
-          title: "Number of Cases"
-        },
-        xAxis: {
-          title: "Month",
-          pointOnColumn: true,
-          dateFormat: "MMM",
-          tickInterval: "auto"
-        },
-        series: {
-          showDot: false,
-          zoomable: true
-        },
-        tooltip: {
-          suffix: ""
+      var dates = [];
+
+      var resolved = [];
+      var confPos = [];
+      var totaltest = [];
+      var dead = [];
+
+      for (var i = 1; i < data.length - 1; i++) {
+        var row = data[i];
+
+        dates[i] = row[0];
+
+        if (!row[5]) {
+          confPos[i] = 0;
+        } else {
+          confPos[i] = row[5];
         }
-      };
-      chart.lineChart(container, data, options);
+        if (!row[6]) {
+          resolved[i] = 0;
+        } else {
+          resolved[i] = row[6];
+        }
+        if (!row[8]) {
+          totaltest[i] = 0;
+        } else {
+          totaltest[i] = row[8];
+        }
+        if (!row[7]) {
+          dead[i] = 0;
+        } else {
+          dead[i] = row[7];
+        }
+        console.log(row);
+      }
+
+      setTimeout(() => {
+        var container = document.getElementById("positive");
+
+        var data = {
+          categories: dates,
+          series: [
+            {
+              name: "Total Tested",
+              data: totaltest
+            },
+            {
+              name: "Confirmed Positives",
+              data: confPos
+            },
+            {
+              name: "Recovered",
+              data: resolved
+            },
+            {
+              name: "Deaths",
+              data: dead
+            }
+          ]
+        };
+        var options = {
+          chart: {
+            width: 1160,
+            height: 540,
+            title: "Status of COVID-19 cases in Ontario"
+          },
+          yAxis: {
+            title: "Number of Cases"
+          },
+          xAxis: {
+            title: "Month",
+            pointOnColumn: true,
+            dateFormat: "MMM",
+            tickInterval: "auto"
+          },
+          series: {
+            showDot: false,
+            zoomable: true
+          },
+          tooltip: {
+            suffix: ""
+          }
+        };
+        chart.lineChart(container, data, options);
+      }, 0.001);
     }, 0.001);
   }
 
