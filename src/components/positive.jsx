383b29f1ec0dc6d6ef87ba7603869cb7c4580853
muiz sohani <muiz.sohani@ontario.ca>
2020-03-26 13:11:58 -0400

    automated all data fields for graph

--- src/components/positive.jsx
+++ src/components/positive.jsx
@@ -29,80 +29,61 @@ class Positive extends Component {
     const data = results.data;
 
     var dates = [];
-    var confPos = [];
 
-    for (var i = 1; i < data.length; i++) {
+    var resolved = [];
+    var confPos = [];
+    var totaltest = [];
+    var dead = [];
+
+    for (var i = 1; i < data.length - 1; i++) {
       var row = data[i];
-      if (row[0]) {
-        dates[i] = row[0];
-      }
-      if (row[5]) {
+
+      dates[i] = row[0];
+
+      if (!row[5]) {
+        confPos[i] = 0;
+      } else {
         confPos[i] = row[5];
       }
+      if (!row[6]) {
+        resolved[i] = 0;
+      } else {
+        resolved[i] = row[6];
+      }
+      if (!row[8]) {
+        totaltest[i] = 0;
+      } else {
+        totaltest[i] = row[8];
+      }
+      if (!row[7]) {
+        dead[i] = 0;
+      } else {
+        dead[i] = row[7];
+      }
+      console.log(row);
     }
-    console.log(dates, confPos);
 
     setTimeout(() => {
       var container = document.getElementById("positive");
-      var conf = confPos;
-      var datez = dates;
+
       var data = {
-        categories: datez,
+        categories: dates,
         series: [
           {
             name: "Total Tested",
-            data: [
-              3.8,
-              5.6,
-              7.0,
-              9.1,
-              12.4,
-              15.3,
-              17.5,
-              17.8,
-              15.0,
-              10.6,
-              6.4,
-              3.7
-            ]
+            data: totaltest
           },
           {
             name: "Confirmed Positives",
-            data: conf
+            data: confPos
           },
           {
             name: "Recovered",
-            data: [
-              -10.3,
-              -9.1,
-              -4.1,
-              4.4,
-              12.2,
-              16.3,
-              18.5,
-              16.7,
-              10.9,
-              4.2,
-              -2.0,
-              -7.5
-            ]
+            data: resolved
           },
           {
             name: "Deaths",
-            data: [
-              -13.2,
-              -13.7,
-              -13.1,
-              -10.3,
-              -6.1,
-              -3.2,
-              0.0,
-              -0.1,
-              -1.8,
-              -4.5,
-              -9.0,
-              -10.9
-            ]
+            data: dead
           }
         ]
       };
@@ -113,7 +94,7 @@ class Positive extends Component {
           title: "Status of COVID-19 cases in Ontario"
         },
         yAxis: {
-          title: ""
+          title: "Number of Cases"
         },
         xAxis: {
           title: "Month",
