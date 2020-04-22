680e6722e69b247e92163c1d9565f83b1584b175
Dani Shulman <dani.shulman@ontario.ca>
2020-04-21 20:41:40 -0400

    fix more

--- src/App.js
+++ src/App.js
@@ -126,6 +126,7 @@ class App extends Component {
         {/* <GoogleTagManager gtmId={'GTM-5G4CS4L'} /> */}
         {this.state.loaded ? (
           <React.Fragment>
+            <div className="ontario-row">
               <p>
                 Last updated:{" "}
                 {preval`process.env.TZ = 'America/Toronto'; module.exports = new Date().toLocaleString();`}
@@ -165,228 +166,232 @@ class App extends Component {
                   {trans.otp.understand[lang]}{" "}
                 </a>
               </div>
-              <hr class="hrule-a" />​
-              <div className="ontario-row">
-                <h2
-                  id="casestatus"
-                  className="item item-2 ontario-columns ontario-small-12"
-                >
-                  {trans.otp.casestatus[lang]}
-                </h2>
-                <div
-                  id="CasesTotal"
-                  className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <p>
-                    {trans.learn.a[lang]}
-                    <a href="#">{trans.learn.b[lang]}</a>
-                    {trans.learn.c[lang]}
-                    <a href="#">{trans.learn.d[lang]}</a>
-                  </p>
-                  <h3 className="margins">{trans.casesTotal.title[lang]}</h3>
-                  <p>{trans.casesTotal.desc[lang]}</p>
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <CasesTotalTable lang={this.state.lang} />
-                    ) : (
-                      <CasesTotal lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
+            </div>
 
-                <div
-                  id="CasesDaily"
-                  className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <h3 className="margins">{trans.casesDaily.title[lang]}</h3>
-                  <p>{trans.casesDaily.desc[lang]}</p>
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <CasesDailyTable lang={this.state.lang} />
-                    ) : (
-                      <CasesDaily lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
-              </div>
-              <hr class="hrule" />​
-              <div className="ontario-row">
-                <div
-                  id="DeathsTotal"
-                  className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <h3 className="margins">{trans.deathsTotal.title[lang]}</h3>
-                  <p>{trans.deathsTotal.desc[lang]}</p>
-
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <DeathsTotalTable lang={this.state.lang} />
-                    ) : (
-                      <DeathsTotal lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
-
-                <div
-                  id="DeathsDaily"
-                  className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <h3 className="margins">{trans.deathsDaily.title[lang]}</h3>
-                  <p>{trans.deathsDaily.desc[lang]}</p>
-
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <DeathsDailyTable lang={this.state.lang} />
-                    ) : (
-                      <DeathsDaily lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
-              </div>
-              <hr class="hrule" />​
-              <div className="ontario-row">
-                <div
-                  id="InICU"
-                  className="item item-6 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <ItemWrapper
-                    title={trans.hospital.active[lang]}
-                    accessToggle={true}
-                  >
-                    {accessible ? (
-                      <ActiveHospitalTable lang={this.state.lang} />
-                    ) : (
-                      <ActiveHospital lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
-                <div
-                  id="Hospital"
-                  className="item item-6 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <ItemWrapper
-                    title={trans.hospital.title[lang]}
-                    accessToggle={true}
-                  >
-                    {accessible ? (
-                      <HospitalTable lang={this.state.lang} />
-                    ) : (
-                      <Hospital lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
-              </div>
-              <hr class="hrule" />​
-              <div id="regbreak" className="item item-6">
-                <h2
-                  id="demo"
-                  className="ontario-margin-bottom-32-! ontario-margin-top-32-!"
-                >
-                  {trans.otp.demo[lang]}
-                </h2>
+            <hr class="hrule-a" />​
+            <div className="ontario-row">
+              <h2
+                id="casestatus"
+                className="item item-2 ontario-columns ontario-small-12"
+              >
+                {trans.otp.casestatus[lang]}
+              </h2>
+              <div
+                id="CasesTotal"
+                className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
+              >
                 <p>
                   {trans.learn.a[lang]}
                   <a href="#">{trans.learn.b[lang]}</a>
                   {trans.learn.c[lang]}
                   <a href="#">{trans.learn.d[lang]}</a>
                 </p>
-                {/* show only on phones */}
-                <div className="ontario-show-for-small-only">
-                  <h3 className="margins">{trans.reg.title[lang]}</h3>
-                  <p>{trans.reg.desc[lang]}</p>
-                  <ItemWrapper accessToggle={false}>
+                <h3 className="margins">{trans.casesTotal.title[lang]}</h3>
+                <p>{trans.casesTotal.desc[lang]}</p>
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <CasesTotalTable lang={this.state.lang} />
+                  ) : (
+                    <CasesTotal lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+
+              <div
+                id="CasesDaily"
+                className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <h3 className="margins">{trans.casesDaily.title[lang]}</h3>
+                <p>{trans.casesDaily.desc[lang]}</p>
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <CasesDailyTable lang={this.state.lang} />
+                  ) : (
+                    <CasesDaily lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+            </div>
+            <hr class="hrule" />​
+            <div className="ontario-row">
+              <div
+                id="DeathsTotal"
+                className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <h3 className="margins">{trans.deathsTotal.title[lang]}</h3>
+                <p>{trans.deathsTotal.desc[lang]}</p>
+
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <DeathsTotalTable lang={this.state.lang} />
+                  ) : (
+                    <DeathsTotal lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+
+              <div
+                id="DeathsDaily"
+                className="item item-2 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <h3 className="margins">{trans.deathsDaily.title[lang]}</h3>
+                <p>{trans.deathsDaily.desc[lang]}</p>
+
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <DeathsDailyTable lang={this.state.lang} />
+                  ) : (
+                    <DeathsDaily lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+            </div>
+            <hr class="hrule" />​
+            <div className="ontario-row">
+              <div
+                id="InICU"
+                className="item item-6 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <ItemWrapper
+                  title={trans.hospital.active[lang]}
+                  accessToggle={true}
+                >
+                  {accessible ? (
+                    <ActiveHospitalTable lang={this.state.lang} />
+                  ) : (
+                    <ActiveHospital lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+              <div
+                id="Hospital"
+                className="item item-6 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <ItemWrapper
+                  title={trans.hospital.title[lang]}
+                  accessToggle={true}
+                >
+                  {accessible ? (
+                    <HospitalTable lang={this.state.lang} />
+                  ) : (
+                    <Hospital lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+            </div>
+            <hr class="hrule" />​
+            <div id="regbreak" className="ontario-row item item-6">
+              <h2
+                id="demo"
+                className="ontario-margin-bottom-32-! ontario-margin-top-32-!"
+              >
+                {trans.otp.demo[lang]}
+              </h2>
+              <p>
+                {trans.learn.a[lang]}
+                <a href="#">{trans.learn.b[lang]}</a>
+                {trans.learn.c[lang]}
+                <a href="#">{trans.learn.d[lang]}</a>
+              </p>
+              {/* show only on phones */}
+              <div className="ontario-show-for-small-only">
+                <h3 className="margins">{trans.reg.title[lang]}</h3>
+                <p>{trans.reg.desc[lang]}</p>
+                <ItemWrapper accessToggle={false}>
+                  <RegBreakTable lang={this.state.lang} />
+                </ItemWrapper>
+              </div>
+              ​{/* show only on all else */}
+              <div className="ontario-hide-for-small-only">
+                <h3 className="margins">{trans.reg.title[lang]}</h3>
+                <p>{trans.reg.desc[lang]}</p>
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
                     <RegBreakTable lang={this.state.lang} />
-                  </ItemWrapper>
-                </div>
-                ​{/* show only on all else */}
-                <div className="ontario-hide-for-small-only">
-                  <h3 className="margins">{trans.reg.title[lang]}</h3>
-                  <p>{trans.reg.desc[lang]}</p>
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <RegBreakTable lang={this.state.lang} />
-                    ) : (
-                      <RegBreak lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
+                  ) : (
+                    <RegBreak lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
               </div>
-              <hr class="hrule" />​
-              <div className="ontario-row">
-                <div
-                  id="CasesByAge"
-                  className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
-                  title={trans.casesByAge.title[lang]}
-                >
-                  <h3 className="margins">{trans.casesByAge.title[lang]}</h3>
-                  <p>{trans.reg.desc[lang]}</p>
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <CasesByAgeTable lang={this.state.lang} />
-                    ) : (
-                      <CasesByAge lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
-                <div
-                  id="CasesBySex"
-                  className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <h3 className="margins">{trans.casesBySex.title[lang]}</h3>
-                  <p>{trans.reg.desc[lang]}</p>
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <CasesBySexTable lang={this.state.lang} />
-                    ) : (
-                      <CasesBySex lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
+            </div>
+            <hr class="hrule" />​
+            <div className="ontario-row">
+              <div
+                id="CasesByAge"
+                className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
+                title={trans.casesByAge.title[lang]}
+              >
+                <h3 className="margins">{trans.casesByAge.title[lang]}</h3>
+                <p>{trans.reg.desc[lang]}</p>
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <CasesByAgeTable lang={this.state.lang} />
+                  ) : (
+                    <CasesByAge lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
               </div>
-              <hr class="hrule" />
+              <div
+                id="CasesBySex"
+                className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <h3 className="margins">{trans.casesBySex.title[lang]}</h3>
+                <p>{trans.reg.desc[lang]}</p>
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <CasesBySexTable lang={this.state.lang} />
+                  ) : (
+                    <CasesBySex lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+            </div>
+            <hr class="hrule" />
+            <div className="ontario-row">
               <h2
                 id="testing"
                 className="ontario-margin-bottom-32-! ontario-margin-top-32-!"
               >
                 {trans.otp.testing[lang]}
               </h2>
-              <div className="ontario-row">
-                <div
-                  id="TotalTests"
-                  className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <p>
-                    {trans.learn.a[lang]}
-                    <a href="#">{trans.learn.b[lang]}</a>
-                    {trans.learn.c[lang]}
-                    <a href="#">{trans.learn.d[lang]}</a>
-                  </p>
-                  <h3 className="margins">{trans.testing.titleA[lang]}</h3>
-                  <p>{trans.testing.descA[lang]}</p>
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <TestsTotalTable lang={this.state.lang} />
-                    ) : (
-                      <TestsTotal lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
-                <div
-                  id="TotalTestsDaily"
-                  className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
-                >
-                  <h3 className="margins">{trans.testing.titleB[lang]}</h3>
-                  <p>{trans.testing.descB[lang]}</p>
-                  <ItemWrapper accessToggle={true}>
-                    {accessible ? (
-                      <TestsDailyTable lang={this.state.lang} />
-                    ) : (
-                      <TestsDaily lang={this.state.lang} />
-                    )}
-                  </ItemWrapper>
-                </div>
+            </div>
+            <div className="ontario-row">
+              <div
+                id="TotalTests"
+                className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <p>
+                  {trans.learn.a[lang]}
+                  <a href="#">{trans.learn.b[lang]}</a>
+                  {trans.learn.c[lang]}
+                  <a href="#">{trans.learn.d[lang]}</a>
+                </p>
+                <h3 className="margins">{trans.testing.titleA[lang]}</h3>
+                <p>{trans.testing.descA[lang]}</p>
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <TestsTotalTable lang={this.state.lang} />
+                  ) : (
+                    <TestsTotal lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
               </div>
-              ​
+              <div
+                id="TotalTestsDaily"
+                className="item item-7 ontario-columns ontario-small-12 ontario-medium-6"
+              >
+                <h3 className="margins">{trans.testing.titleB[lang]}</h3>
+                <p>{trans.testing.descB[lang]}</p>
+                <ItemWrapper accessToggle={true}>
+                  {accessible ? (
+                    <TestsDailyTable lang={this.state.lang} />
+                  ) : (
+                    <TestsDaily lang={this.state.lang} />
+                  )}
+                </ItemWrapper>
+              </div>
+            </div>
+            ​
           </React.Fragment>
         ) : (
           <React.Fragment>
