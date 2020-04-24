0e120c5a1d4a015f349f0cd024a1dc83ba8ce1ac
muiz sohani <muiz.sohani@ontario.ca>
2020-04-24 13:02:27 -0400

    set an if statement to determine which language to show the tooltips in

--- src/components/casesByAge.jsx
+++ src/components/casesByAge.jsx
@@ -78,122 +78,242 @@ class AgeBreak extends Component {
         return item[dict.deaths];
       });
 
-    this.setState({
-      series: [
-        {
-          name: trans.casesByAge.active[this.props.lang],
-          data: active,
-        },
-        {
-          name: trans.casesByAge.resolved[this.props.lang],
-          data: reso,
-        },
-        {
-          name: trans.casesByAge.fatal[this.props.lang],
-          data: fatal,
-        },
-      ],
-      options: {
-        legend: legend,
-        tooltip: {
-          followCursor: true,
-          style: { ...labelStyle },
-          x: {
-            formatter: function (val, { series, seriesIndex, dataPointIndex }) {
-              if (dataPointIndex === 0) {
-                return "Age under 20";
-              }
-              if (dataPointIndex === 9) {
-                return "Age unknown";
-              }
-              return "Age " + val;
-            },
+    if (this.props.lang === "en") {
+      this.setState({
+        series: [
+          {
+            name: trans.casesByAge.active[this.props.lang],
+            data: active,
           },
-        },
-        responsive: responsiveFun().map((item) => {
-          item.options.chart.height = "350px";
-          return item;
-        }),
-        chart: {
-          height: 650,
-          width: "100%",
-          type: "bar",
-          stacked: true,
-          zoom: { enabled: true },
-        },
-        //title: { text: "Breakdown by Age and Sex" },
-        dataLabels: {
-          enabled: true,
-          textAnchor: "start",
-          offsetX: 25,
-          style: { ...labelStyle },
-          formatter: function (value, { seriesIndex, dataPointIndex, w }) {
-            let indices = w.config.series.map((item, i) => i);
-            indices = indices.filter(
-              (i) =>
-                !w.globals.collapsedSeriesIndices.includes(i) &&
-                w.config.series[i].data[dataPointIndex] > 0
-            );
-            if (seriesIndex === indices[indices.length - 1])
-              return w.globals.stackedSeriesTotals[dataPointIndex];
-            return "";
+          {
+            name: trans.casesByAge.resolved[this.props.lang],
+            data: reso,
           },
-        },
-        // fill: {
-        //   type: ["solid", "pattern"],
-        //   opacity: 1,
-        //   pattern: {
-        //     style: "slantedLines",
-        //   },
-        // },
-
-        colors: ["#00B2E3", "#8dc63f", "#4d4d4d"],
-        plotOptions: {
-          bar: {
-            horizontal: true,
-            dataLabels: {
-              position: "top",
-            },
+          {
+            name: trans.casesByAge.fatal[this.props.lang],
+            data: fatal,
           },
-        },
-        stroke: {
-          width: 2,
-          colors: ["#fff"],
-        },
-        yaxis: {
-          title: {
-            text: trans.age[this.props.lang],
-          },
-          labels: {
+        ],
+        options: {
+          legend: legend,
+          tooltip: {
+            followCursor: true,
             style: { ...labelStyle },
+            x: {
+              formatter: function (
+                val,
+                { series, seriesIndex, dataPointIndex }
+              ) {
+                if (dataPointIndex === 0) {
+                  return "Age under 20";
+                }
+                if (dataPointIndex === 9) {
+                  return "Age unknown";
+                }
+                return "Age " + val;
+              },
+            },
           },
-        },
-        xaxis: {
-          title: {
-            offsetY: 20,
-            text: trans.casesTotal.yaxis[this.props.lang],
+          responsive: responsiveFun().map((item) => {
+            item.options.chart.height = "350px";
+            return item;
+          }),
+          chart: {
+            height: 650,
+            width: "100%",
+            type: "bar",
+            stacked: true,
+            zoom: { enabled: true },
           },
-          categories: [
-            trans.casesByAge.under20[this.props.lang],
-            "20-29",
-            "30-39",
-            "40-49",
-            "50-59",
-            "60-69",
-            "70-79",
-            "80-89",
-            "90-99",
-            trans.casesByAge.unk[this.props.lang],
-          ],
-          labels: {
-            ...lgXaxisLabels,
+          //title: { text: "Breakdown by Age and Sex" },
+          dataLabels: {
+            enabled: true,
+            textAnchor: "start",
+            offsetX: 25,
+            style: { ...labelStyle },
+            formatter: function (value, { seriesIndex, dataPointIndex, w }) {
+              let indices = w.config.series.map((item, i) => i);
+              indices = indices.filter(
+                (i) =>
+                  !w.globals.collapsedSeriesIndices.includes(i) &&
+                  w.config.series[i].data[dataPointIndex] > 0
+              );
+              if (seriesIndex === indices[indices.length - 1])
+                return w.globals.stackedSeriesTotals[dataPointIndex];
+              return "";
+            },
           },
-        },
-      },
-    });
+          // fill: {
+          //   type: ["solid", "pattern"],
+          //   opacity: 1,
+          //   pattern: {
+          //     style: "slantedLines",
+          //   },
+          // },
 
-    var unknowns = a + b + c;
-    this.setState({ ready: true, unk: unknowns });
+          colors: ["#00B2E3", "#8dc63f", "#4d4d4d"],
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              dataLabels: {
+                position: "top",
+              },
+            },
+          },
+          stroke: {
+            width: 2,
+            colors: ["#fff"],
+          },
+          yaxis: {
+            title: {
+              text: trans.age[this.props.lang],
+            },
+            labels: {
+              style: { ...labelStyle },
+            },
+          },
+          xaxis: {
+            title: {
+              offsetY: 20,
+              text: trans.casesTotal.yaxis[this.props.lang],
+            },
+            categories: [
+              trans.casesByAge.under20[this.props.lang],
+              "20-29",
+              "30-39",
+              "40-49",
+              "50-59",
+              "60-69",
+              "70-79",
+              "80-89",
+              "90-99",
+              trans.casesByAge.unk[this.props.lang],
+            ],
+            labels: {
+              ...lgXaxisLabels,
+            },
+          },
+        },
+      });
+    } else {
+      this.setState({
+        series: [
+          {
+            name: trans.casesByAge.active[this.props.lang],
+            data: active,
+          },
+          {
+            name: trans.casesByAge.resolved[this.props.lang],
+            data: reso,
+          },
+          {
+            name: trans.casesByAge.fatal[this.props.lang],
+            data: fatal,
+          },
+        ],
+        options: {
+          legend: legend,
+          tooltip: {
+            followCursor: true,
+            style: { ...labelStyle },
+            x: {
+              formatter: function (
+                val,
+                { series, seriesIndex, dataPointIndex }
+              ) {
+                if (dataPointIndex === 0) {
+                  return "Age under 20 FR";
+                }
+                if (dataPointIndex === 9) {
+                  return "Age unknown FR";
+                }
+                return "Age FR " + val;
+              },
+            },
+          },
+          responsive: responsiveFun().map((item) => {
+            item.options.chart.height = "350px";
+            return item;
+          }),
+          chart: {
+            height: 650,
+            width: "100%",
+            type: "bar",
+            stacked: true,
+            zoom: { enabled: true },
+          },
+          //title: { text: "Breakdown by Age and Sex" },
+          dataLabels: {
+            enabled: true,
+            textAnchor: "start",
+            offsetX: 25,
+            style: { ...labelStyle },
+            formatter: function (value, { seriesIndex, dataPointIndex, w }) {
+              let indices = w.config.series.map((item, i) => i);
+              indices = indices.filter(
+                (i) =>
+                  !w.globals.collapsedSeriesIndices.includes(i) &&
+                  w.config.series[i].data[dataPointIndex] > 0
+              );
+              if (seriesIndex === indices[indices.length - 1])
+                return w.globals.stackedSeriesTotals[dataPointIndex];
+              return "";
+            },
+          },
+          // fill: {
+          //   type: ["solid", "pattern"],
+          //   opacity: 1,
+          //   pattern: {
+          //     style: "slantedLines",
+          //   },
+          // },
+
+          colors: ["#00B2E3", "#8dc63f", "#4d4d4d"],
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              dataLabels: {
+                position: "top",
+              },
+            },
+          },
+          stroke: {
+            width: 2,
+            colors: ["#fff"],
+          },
+          yaxis: {
+            title: {
+              text: trans.age[this.props.lang],
+            },
+            labels: {
+              style: { ...labelStyle },
+            },
+          },
+          xaxis: {
+            title: {
+              offsetY: 20,
+              text: trans.casesTotal.yaxis[this.props.lang],
+            },
+            categories: [
+              trans.casesByAge.under20[this.props.lang],
+              "20-29",
+              "30-39",
+              "40-49",
+              "50-59",
+              "60-69",
+              "70-79",
+              "80-89",
+              "90-99",
+              trans.casesByAge.unk[this.props.lang],
+            ],
+            labels: {
+              ...lgXaxisLabels,
+            },
+          },
+        },
+      });
+    }
+
     this.setState({ ready: true });
   }
 
